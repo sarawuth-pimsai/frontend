a53450e943a4eaa536fae6672a29bf46249e9e04
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-03-18 16:57:40 +0700

    Change word

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sarawuth Pimsai
         </a>
       </header>
     </div>
