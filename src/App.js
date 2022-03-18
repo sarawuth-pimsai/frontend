52d7c66e57cbb6545c65eb0e9cccc370b5a8de8e
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-03-18 20:16:31 +0700

    Debug channel id

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Sarawuth learn react
+          Sarawuth pimsai learn react
         </a>
       </header>
     </div>
