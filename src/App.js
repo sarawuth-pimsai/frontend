4f2f6c6ed8b223aedd9bb5c93f50562c267e7de6
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-04-20 14:40:31 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world hotfix #5 learn react
+          Hello world hotfix #6 learn react
         </a>
       </header>
     </div>
