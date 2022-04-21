9464da579d1571205294cd809da224e6c57b688c
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-04-21 11:10:14 +0700

    commit

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -3028,8 +3028,32 @@ async function run(){
     try{
         const toke = core.getInput('token')
         const branchesResult = await cmd('git', 'branch')
-        console.log(branchesResult)
-        core.setOutput("values", branchesResult)
+        let diffCommits = await cmd("git", "log", "--format=%H", "origin/main...HEAD")
+        let tags = await cmd("git", "describe", "--tags", "--abbrev=0");
+        const commitBase = await cmd("git", "rev-list", "-n1", `${tags[0]}`);
+        const commitHead = await cmd("git", "rev-list", "-n1", "HEAD");
+        const commitMergeBase = await cmd(
+            "git",
+            "merge-base",
+            `-a`,
+            `${commitBase[0]}`,
+            `${commitHead[0]}`
+            );
+            let commands = [
+            "git",
+            "log",
+            "--merges",
+            "--first-parent",
+            "--reverse",
+            "--format='%P %s'",
+        ];
+        if (commitMergeBase[0] === commitHead[0]) {
+            commands.push(`${commitMergeBase[0]}...${commitBase[0]}`);
+        } else {
+            commands.push(`${commitMergeBase[0]}...${commitHead[0]}`);
+        }
+        const pullRequests = await cmd(...commands);
+        core.setOutput("values", {branchesResult, diffCommits, tags, commitBase, commitHead, commitMergeBase, pullRequests})
     }catch(error){
         core.error(error)
         core.setFailed(error)
