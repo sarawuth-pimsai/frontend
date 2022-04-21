323040f937fc1c700d1e943deae3968999eefe57
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-04-21 10:46:35 +0700

    Script

---
+++ src/actions/test/main.js
@@ -0,0 +1,22 @@
+import { getExecOutput } from "@actions/exec";
+const core = require("@actions/core");
+
+export async function cmd(commandLine, ...args) {
+    info(`The command ${commandLine} ${args.join(" ")}`);
+    const options = { ignoreReturnCode: true, silent: true };
+    const output = await getExecOutput(commandLine, args, options);
+    if (output.exitCode > 0) throw new Error(output.stderr);
+    return output.stdout.split("\n").filter((o) => o !== "");
+}
+async function run(){
+    try{
+        const toke = core.getInput('token')
+        const branchesResult = await cmd('git', 'branch')
+        console.log(branchesResult)
+        core.setOutput("values", branchesResult)
+    }catch(error){
+        core.error(error)
+        core.setFailed(error)
+    }
+}
+run()
